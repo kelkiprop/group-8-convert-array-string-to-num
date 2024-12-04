e2804ce8b4f69cd66e3d521cfadabfea572c9c67
kelvin_kiprop <kelkiprop@gmail.com>
2024-12-04 15:04:37 +0300

    strToNum array

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-// the array with strings 
-const stringArray = ["1", "2", "3"];
+let stringArray = ["1", "2", "3"]; // declaring the variable
+function convertArray() {
+    let numberArray = stringArray.map(str => Number(str)); // Convert each string to a number
+    console.log(numberArray);
+}
+convertArray();
